3aa03c3aee53ddba26120e3097b6ef14c5c40ab2
Crystal Lin <crystaltlin@Crystals-MBP-2.fios-router.home>
2019-10-30 23:50:01 -0400

    Updates

---
+++ precache-manifest.20b392754677ccc207a2277de36d3059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e42477ce2814a6435a85a6a21189dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4732e8cd5e7a130b5465",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6ed79ec1bce2e116b5b6",
+    "url": "/static/css/main.9fcaf997.chunk.css"
+  },
+  {
+    "revision": "4732e8cd5e7a130b5465",
+    "url": "/static/js/2.840d6180.chunk.js"
+  },
+  {
+    "revision": "6ed79ec1bce2e116b5b6",
+    "url": "/static/js/main.7e64aba3.chunk.js"
+  },
+  {
+    "revision": "339709f8d42c481bbd86",
+    "url": "/static/js/runtime-main.a0d37409.js"
+  }
+]);
